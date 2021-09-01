44ef852122d894e5890860f17d2ecc071d675509
Giuseppe Cardillo <37176275+dnafinder@users.noreply.github.com>
2021-09-01 17:23:20 +0200

    Update roc.m
    deep revision

--- roc.m
+++ roc.m
@@ -51,8 +51,39 @@ if all(x(:,2)==1)
     error('Warning: there are only unhealthy subjects!')
 end
 
+ButtonName = questdlg('Do you want to input the true prevalence?', 'Prevalence Question', 'Yes', 'No', 'No');
+if strcmp(ButtonName,'Yes')
+    ButtonName = questdlg('Do you want to input the true prevalence as:', 'Prevalence Question', 'Ratio', 'Probability', 'Ratio');
+    switch ButtonName
+        case 'Ratio'
+            prompt={'Enter the Numerator or the prevalence ratio:','Enter the denominator or the prevalence ratio:'};
+            name='Input for Ratio prevalence';
+            Ratio=str2double(inputdlg(prompt,name));
+            POD=Ratio(1)/diff(Ratio); %prior odds
+        case 'Probability'
+            prompt={'Enter the prevalence probability comprised between 0 and 1:'};
+            name='Input for prevalence';
+            pr=str2double(inputdlg(prompt,name));
+            POD=pr/(1-pr); %prior odds
+    end
+    clear ButtonName prompt name Ratio pr
+end
+if exist('POD','var')
+    methods={'Accuracy','Balanced Accuracy','F1-measure','G-Measure','Matthews Index'};
+else
+    methods={'Accuracy','Balanced Accuracy'};
+end
+type=[];
+while isempty(type)
+    type=listdlg('PromptString','Select the Efficency method calculation:','ListSize',[300 150],...
+    'Name','Disposable methods', 'SelectionMode','single',...
+    'ListString',methods);
+end
+clear methods
+
 tr=repmat('-',1,100);
 z=sortrows(x,1);
+z(z(:,1)<=0,:)=[];
 if threshold==0
     labels=unique(z(:,1));%find unique values in z
 else
@@ -62,7 +93,10 @@ end
 clear z
 
 ll=length(labels); %count unique value
-a=zeros(ll,2); c=zeros(ll,1);%array preallocation
+a=zeros(ll,2); c=zeros(ll,1) ;%array preallocation
+if type>2
+    d=zeros(ll,2);
+end
 ubar=mean(x(x(:,2)==1),1); %unhealthy mean value
 hbar=mean(x(x(:,2)==0),1); %healthy mean value
 N=length(x);
@@ -80,38 +114,70 @@ for K=1:ll
     end
     M=[TP FP;FN TN];
     a(K,:)=diag(M)'./sum(M); %Sensitivity and Specificity
-    c(K)=trace(M)/N; %Efficiency
+    if type>2
+        PLR=a(K,1)/(1-a(K,2)); 
+        PPV=1/(1+1/(PLR*POD));
+        NLR=(1-a(K,1))/(a(K,2));
+        PPN=1/(1+NLR*POD);
+        d(K,:)=[PPV PPN];
+    end
+    switch type
+        case 1
+            c(K)=trace(M)/N; %accuracy
+        case 2
+            c(K)=mean(a(K,:));%Balanced accuracy
+        case 3
+            if all([a(K,1) PPV])
+                c(K)=harmmean([a(K,1) PPV]);%F1-Measure
+            else
+                c(K)=NaN;
+            end
+        case 4
+            if all([a(K,1) PPV]) && all([a(K,1) PPV]>0)
+                c(K)=geomean([a(K,1) PPV]);%G-Measure
+            else
+                c(K)=NaN;
+            end
+        case 5
+            J=sum(a(K,:))-1; PSI=PPV+PPN-1;
+            if all([J PSI]) && all([J PSI]>0)
+                c(K)=geomean([J PSI]);%Matthews
+            else
+                c(K)=NaN;
+            end
+    end
 end
-clear ll K TP FP FN TN M N
+clear ll K TP FP FN TN M N type PLR PPV NLR PPN type
 b=[a(:,1)./(1-a(:,2)) (1-a(:,1))./a(:,2)];
 
 xroc=1-a(:,2); yroc=a(:,1); %ROC points
 if hbar>ubar
     xroc=flipud(xroc); yroc=flipud(yroc); %ROC points
 end
-clear ubar hbar
-if ~isequal([xroc(end) yroc(end)],[1 1])
-    xroc(end+1)=1; yroc(end+1)=1;
+
+f=polyfit(1:1:length(yroc),yroc',1);
+if sign(f(1))==-1
+    if ~isequal([xroc(1) yroc(1)],[1 1])
+        xroc=[1;xroc]; yroc=[1;yroc];
+    end
+    if ~isequal([xroc(end) yroc(end)],[0 0])
+        xroc(end+1)=0; yroc(end+1)=0;
+    end
+elseif sign(f(1))==1
+    if ~isequal([xroc(1) yroc(1)],[0 0])
+        xroc=[0;xroc]; yroc=[0;yroc];
+    end
+    if ~isequal([xroc(end) yroc(end)],[1 1])
+        xroc(end+1)=1; yroc(end+1)=1;
+    end
 end
 
-ft = fittype( '1-1/((1+(x/C)^B)^E)', 'independent', 'x', 'dependent', 'y' );
-opts = fitoptions( 'Method', 'NonlinearLeastSquares' );
-opts.Display = 'Off';
-opts.Lower = [0 0 0];
-opts.Robust = 'Bisquare';
-opts.StartPoint = [1 0.5 1];
-opts.Upper = [Inf 1 Inf];
-rocfit = fit(xroc,yroc,ft,opts);
-myfun=@(x,rf) -((rf(2)./x)^(-rf(1)) + 1)^(-rf(3)) - x + 1 ;
-cross=fzero(@(x) myfun(x,coeffvalues(rocfit)),1);
+STATS=mwwtest(x(x(:,2)==1)',x(x(:,2)==0)');
+%use Mann-Withney-Wilcoxon U stats
+Area=max(STATS.U)/prod(STATS.n);
 
-xfit=linspace(0,1,500);
-yfit=rocfit(xfit);
-clear ft opt
-Area=trapz(xfit,yfit); %estimate the area under the curve
 %standard error of area
-lu=length(x(x(:,2)==1)); %number of unhealthy subjects
-lh=length(x(x(:,2)==0)); %number of healthy subjects
+lu=STATS.n(1); lh=STATS.n(2); 
 Area2=Area^2; Q1=Area/(2-Area); Q2=2*Area2/(1+Area);
 V=(Area*(1-Area)+(lu-1)*(Q1-Area2)+(lh-1)*(Q2-Area2))/(lu*lh);
 Serror=realsqrt(V);
@@ -129,7 +195,6 @@ if nargout
     ROCout.ci=ci; % 95% Confidence interval
     ROCout.xr=xroc; %graphic x points
     ROCout.yr=yroc; %graphic y points
-    ROCout.rocfit=rocfit;
 end
 
 clear lu lh Area2 Q1 Q2 V
@@ -162,46 +227,27 @@ if verbose==1
         disp('The area is not statistically greater than 0.5')
     end
     disp(' ')
+    
     %display graph
     H=figure;
     set(H,'Position',[4 402 560 420])
     axis square; hold on
-    plot(xfit,yfit,'marker','none','linestyle','-','color','k','linewidth',2);
-    xfit=linspace(0,cross,500);
-    yfit=rocfit(xfit);
-    clear rocfit
-    patch(xfit,yfit,'g','FaceAlpha',0.5)
+    sh=stairs(xroc,yroc,'color','b','linewidth',2);
+    fill([sh.XData(1),repelem(sh.XData(2:end),2)],[repelem(sh.YData(1:end-1),2),sh.YData(end)],'g','FaceAlpha',0.5)
+    clear sh
     patch([0 1 1],[0 0 1],'r','FaceAlpha',0.5)
     set(gca,'Xtick',0:0.1:1)
     grid on
-    plot(xfit,yfit,'marker','none','linestyle','-','color','r','linewidth',2);
-    H1=plot(xroc,yroc,'bo');
-    set(H1,'markersize',6,'markeredgecolor','b','markerfacecolor','b')
     hold off
     xlabel('False positive rate (1-Specificity)')
     ylabel('True positive rate (Sensitivity)')
     title(sprintf('ROC curve (AUC=%0.4f)',Area))
-
-    clear Area Serror ci str SAUC Serror xroc yroc H H1 xfit yfit
+    
+    clear Area Serror ci str SAUC Serror xroc yroc H 
+    
     if p<=alpha
         clear p
-        ButtonName = questdlg('Do you want to input the true prevalence?', 'Prevalence Question', 'Yes', 'No', 'No');
-        if strcmp(ButtonName,'Yes')  
-            ButtonName = questdlg('Do you want to input the true prevalence as:', 'Prevalence Question', 'Ratio', 'Probability', 'Ratio');
-            switch ButtonName
-                case 'Ratio'
-                    prompt={'Enter the Numerator or the prevalence ratio:','Enter the denominator or the prevalence ratio:'};
-                    name='Input for Ratio prevalence';
-                    Ratio=str2double(inputdlg(prompt,name));
-                    POD=Ratio(1)/diff(Ratio); %prior odds
-                case 'Probability'
-                    prompt={'Enter the prevalence probability comprised between 0 and 1:'};
-                    name='Input for prevalence';
-                    pr=str2double(inputdlg(prompt,name));
-                    POD=pr/(1-pr); %prior odds
-            end
-            clear ButtonName prompt name Ratio pr
-            d=[1./(1+1./(b(:,1).*POD)) 1./(1+(b(:,2).*POD))];
+        if exist('POD','var')
             d((a(:,1)==0 & a(:,2)==1),1)=NaN;
             d((a(:,1)==1 & a(:,2)==0),2)=NaN;
             matrix=[labels'; a(:,1)'; a(:,2)';c';b(:,1)'; b(:,2)';d(:,1)'.*100; d(:,2)'.*100;]';
@@ -212,7 +258,6 @@ if verbose==1
                 disp(array2table(matrix,'VariableNames',{'Cut_off','Sensitivity','Specificity','Efficiency','PLR','NLR','Pos_pred','Neg_Pred'}))
             end
         else
-            clear ButtonName
             matrix=[labels'; a(:,1)'; a(:,2)';c';b(:,1)'; b(:,2)']';
             clear a b c d labels
             if verbose
@@ -222,106 +267,103 @@ if verbose==1
             end
         end
         clear tr
-        
-        if matrix(1,2)>matrix(end,2) %Max sensitivity cut-off
-            CSe=mean(matrix(matrix(:,1)==max(matrix(matrix(:,2)==max(matrix(:,2)))),1)); 
-        else
-            CSe=mean(matrix(matrix(:,1)==min(matrix(matrix(:,2)==max(matrix(:,2)))),1)); 
-        end
-        if matrix(1,3)>matrix(end,3) %Max specificity cut-off
-            CSp=mean(matrix(matrix(:,1)==max(matrix(matrix(:,3)==max(matrix(:,3)))),1)); 
-        else
-            CSp=mean(matrix(matrix(:,1)==min(matrix(matrix(:,3)==max(matrix(:,3)))),1)); 
-        end
-        CEff=mean(matrix(matrix(:,1)==min(matrix(matrix(:,4)==max(matrix(:,4)))),1)); %Max efficiency cut-off
-        CPlr=mean(matrix(matrix(:,5)==max(matrix(~isnan(matrix(:,5)) & ~isinf(matrix(:,5)),5)),1)); %Max PLR cut-off
-        CNlr=mean(matrix(matrix(:,6)==max(matrix(~isnan(matrix(:,6)),6)),1)); %Max NLR cut-off
-        
-        z=min(matrix(:,1));
-        if z<0
-            COEFF=abs(z)+1;
-        else
-            COEFF=0;
-        end
-        clear z
-        
-        mM=min(matrix(:,1))+COEFF; M=mean(matrix(:,1))+COEFF; MM=max(matrix(:,1))+COEFF;
-        ft = fittype( '1-1/((1+(x/C)^B)^E)', 'independent', 'x', 'dependent', 'y' );
-        opts = fitoptions( 'Method', 'NonlinearLeastSquares' );
-        opts.Display = 'Off';
-        opts.StartPoint = [0 M 0];
-        if matrix(1,2)>matrix(end,2)
-            opts.Lower = [-Inf mM 0];
-            opts.Upper = [0 MM Inf];
-        else
-            opts.Lower = [0 mM 0];
-            opts.Upper = [Inf MM Inf];
-        end
-        fitSe = fit(matrix(:,1)+COEFF,matrix(:,2), ft, opts );
-        if matrix(1,3)>matrix(end,3)
-            opts.Lower = [-Inf mM 0];
-            opts.Upper = [0 MM Inf];
-        else
-            opts.Lower = [0 mM 0];
-            opts.Upper = [Inf MM Inf];
-        end
-        fitSp = fit(matrix(:,1)+COEFF,matrix(:,3), ft, opts );
-        
-        ft = fittype('D+(A-D)/((1+(x/C)^B)^E)', 'independent', 'x', 'dependent', 'y' );
-        opts.Lower = zeros(1,5);
-        opts.StartPoint = [min(matrix(:,4)) 1 M max(matrix(:,4)) 1];
-        opts.Upper = [1 Inf MM 1 Inf];
-        fitEff = fit(matrix(:,1)+COEFF,matrix(:,4), ft, opts );
-
-        myfun=@(x,se,sp) (-1./(1+(x./se(2)).^se(1)).^se(3))+(1./(1+(x./sp(2)).^sp(1)).^sp(3));
-        SeSp=fzero(@(x) myfun(x,coeffvalues(fitSe),coeffvalues(fitSp)),M);
-        
-        clear mM M MM ft opts myfun
-
-        xg=linspace(0,max(matrix(:,1))+COEFF,500);
-        H2=figure;
-        set(H2,'Position',[570 402 868 420])
-        hold on
-        H=ones(1,9);
-        c=[0 0 1;1 0 0; 0 1 0; 0 0 0.1724; 1 0.1034 0.7241; 1 0.8276 0];
-        H(1) = plot(xg,feval(fitSe,xg),'marker','none','linestyle','-','color',c(2,:),'linewidth',2);
-        H(2)=plot([CSe CSe]+COEFF,[0 1],'marker','none','linestyle','--','color',c(2,:),'linewidth',2);
-        H(3) = plot(xg,feval(fitSp,xg),'marker','none','linestyle','-','color',c(3,:),'linewidth',2);
-        H(4)=plot([CSp CSp]+COEFF,[0 1],'marker','none','linestyle','--','color',c(3,:),'linewidth',2);
-        H(5) = plot(xg,feval(fitEff,xg),'marker','none','linestyle','-','color',c(1,:),'linewidth',2);
-        H(6)=plot([CEff CEff]+COEFF,[0 1],'marker','none','linestyle','--','color',c(1,:),'linewidth',2);
-        H(7)=plot([SeSp SeSp],[0 1],'marker','none','linestyle','--','color',c(6,:),'linewidth',2);
-        H(8)=plot([CPlr CPlr]+COEFF,[0 1],'marker','none','linestyle','--','color',c(5,:),'linewidth',2);
-        H(9)=plot([CNlr CNlr]+COEFF,[0 1],'marker','none','linestyle','--','color',c(4,:),'linewidth',2);
-        xlabel('Test cut-off')
-        ylabel('Percent')
-        hold off
-        legend(H,...
-            'Sensitivity',sprintf('Max Sensitivity cutoff: %0.4f',CSe),...
-            'Specificity',sprintf('Max Specificity cutoff: %0.4f',CSp),...
-            'Efficiency',sprintf('Max Efficiency cutoff: %0.4f',CEff),...
-            sprintf('Cost Effective cutoff: %0.4f',SeSp-COEFF),...
-            sprintf('Max PLR: %0.4f',CPlr),sprintf('Max NLR: %0.4f',CNlr),...
-            'Location','BestOutside')
-        axis([xg(1) xg(end) 0 1.1])
-        if COEFF~=0
-            xt=get(gca,'XTick'); Lxt=length(xt);
-            xtl=cell(1,Lxt);
-            for I=1:Lxt
-                xtl{I}=sprintf('%0.2f',xt(I)-COEFF);
+        ROCout.matrix=matrix;
+        if length(matrix(:,1))>2
+            if hbar<ubar 
+                CSe=mean(matrix(matrix(:,1)==min(matrix(matrix(:,2)==max(matrix(:,2)))),1));%Max sensitivity cut-off
+                CSp=mean(matrix(matrix(:,1)==min(matrix(matrix(:,3)==max(matrix(:,3)))),1));%Max specificity cut-off
+            else
+                CSe=mean(matrix(matrix(:,1)==max(matrix(matrix(:,2)==max(matrix(:,2)))),1));%Max sensitivity cut-off
+                CSp=mean(matrix(matrix(:,1)==max(matrix(matrix(:,3)==max(matrix(:,3)))),1));%Max specificity cut-off
             end
-            set(gca,'XTick',xt,'XTickLabel',xtl)
-            clear xt xtl I Lxt
+            CEff=mean(min(matrix(matrix(:,4)==max(matrix(isfinite(matrix(:,4)),4)),1))); %Max efficiency cut-off
+            CPlr=mean(min(matrix(matrix(:,5)==max(matrix(isfinite(matrix(:,5)),5)),1))); %Max PLR cut-off
+            CNlr=mean(min(matrix(matrix(:,6)==min(matrix(isfinite(matrix(:,6)),6)),1))); %Min NLR cut-off
+            
+            z=min(matrix(:,1));
+            if z<0
+                COEFF=abs(z)+1;
+            else
+                COEFF=0;
+            end
+            clear z
+            
+            mM=min(matrix(:,1))+COEFF; M=mean(matrix(:,1))+COEFF; MM=max(matrix(:,1))+COEFF;
+            ft = fittype( '1-1/((1+(x/C)^B)^E)', 'independent', 'x', 'dependent', 'y' );
+            opts = fitoptions( 'Method', 'NonlinearLeastSquares' );
+            opts.Display = 'Off';
+            opts.StartPoint = [0 M 0];
+            if matrix(1,2)>matrix(end,2)
+                opts.Lower = [-Inf mM 0];
+                opts.Upper = [0 MM Inf];
+            else
+                opts.Lower = [0 mM 0];
+                opts.Upper = [Inf MM Inf];
+            end
+            fitSe = fit(matrix(:,1)+COEFF,matrix(:,2), ft, opts );
+            if matrix(1,3)>matrix(end,3)
+                opts.Lower = [-Inf mM 0];
+                opts.Upper = [0 MM Inf];
+            else
+                opts.Lower = [0 mM 0];
+                opts.Upper = [Inf MM Inf];
+            end
+            fitSp = fit(matrix(:,1)+COEFF,matrix(:,3), ft, opts );
+            
+            [xData, yData] = prepareCurveData(matrix(:,1)+COEFF,matrix(:,4));
+            ft = fittype( 'smoothingspline' );
+            fitEff = fit( xData, yData, ft);
+            clear xData yData ft ok
+            
+            myfun=@(x,se,sp) (-1./(1+(x./se(2)).^se(1)).^se(3))+(1./(1+(x./sp(2)).^sp(1)).^sp(3));
+            SeSp=fzero(@(x) myfun(x,coeffvalues(fitSe),coeffvalues(fitSp)),M);
+            
+            clear mM M MM ft opts myfun
+            
+            xg=linspace(0,max(matrix(:,1))+COEFF,500);
+            H2=figure;
+            set(H2,'Position',[570 402 868 420])
+            hold on
+            H=ones(1,9);
+            c=[0 0 1;1 0 0; 0 1 0; 0 0 0.1724; 1 0.1034 0.7241; 1 0.8276 0];
+            H(1) = plot(xg,feval(fitSe,xg),'marker','none','linestyle','-','color',c(2,:),'linewidth',2);
+            H(2)=plot([CSe CSe]+COEFF,[0 1],'marker','none','linestyle','--','color',c(2,:),'linewidth',2);
+            H(3) = plot(xg,feval(fitSp,xg),'marker','none','linestyle','-','color',c(3,:),'linewidth',2);
+            H(4)=plot([CSp CSp]+COEFF,[0 1],'marker','none','linestyle','--','color',c(3,:),'linewidth',2);
+            H(5) = plot(xg,feval(fitEff,xg),'marker','none','linestyle','-','color',c(1,:),'linewidth',2);
+            H(6)=plot([CEff CEff]+COEFF,[0 1],'marker','none','linestyle','--','color',c(1,:),'linewidth',2);
+            H(7)=plot([SeSp SeSp],[0 1],'marker','none','linestyle','--','color',c(6,:),'linewidth',2);
+            H(8)=plot([CPlr CPlr]+COEFF,[0 1],'marker','none','linestyle','--','color',c(5,:),'linewidth',2);
+            H(9)=plot([CNlr CNlr]+COEFF,[0 1],'marker','none','linestyle','--','color',c(4,:),'linewidth',2);
+            xlabel('Test cut-off')
+            ylabel('Percent')
+            hold off
+            legend(H,...
+                'Sensitivity',sprintf('Max Sensitivity cutoff: %0.4f',CSe),...
+                'Specificity',sprintf('Max Specificity cutoff: %0.4f',CSp),...
+                'Efficiency',sprintf('Max Efficiency cutoff: %0.4f',CEff),...
+                sprintf('Cost Effective cutoff: %0.4f',SeSp-COEFF),...
+                sprintf('Max PLR: %0.4f',CPlr),sprintf('Min NLR: %0.4f',CNlr),...
+                'Location','BestOutside')
+            axis([xg(1) xg(end) 0 1.1])
+            if COEFF~=0
+                xt=get(gca,'XTick'); Lxt=length(xt);
+                xtl=cell(1,Lxt);
+                for I=1:Lxt
+                    xtl{I}=sprintf('%0.2f',xt(I)-COEFF);
+                end
+                set(gca,'XTick',xt,'XTickLabel',xtl)
+                clear xt xtl I Lxt
+            end
+            
+            fprintf('1) Max Sensitivity Cut-off point= %0.4f\n',CSe)
+            fprintf('2) Max Specificity Cut-off point= %0.4f\n',CSp)
+            fprintf('3) Cost effective Cut-off point (Sensitivity=Specificity=%0.4f)= %0.4f\n',fitSe(SeSp-COEFF),SeSp-COEFF)
+            fprintf('4) Max Efficiency Cut-off point= %0.4f\n',CEff)
+            fprintf('5) Max PLR Cut-off point= %0.4f\n',CPlr)
+            fprintf('6) Min NLR Cut-off point= %0.4f\n',CNlr)
+            m=[CSe CSp SeSp CEff CPlr CNlr];
         end
-        
-        fprintf('1) Max Sensitivity Cut-off point= %0.4f\n',CSe)
-        fprintf('2) Max Specificity Cut-off point= %0.4f\n',CSp)
-        fprintf('3) Cost effective Cut-off point (Sensitivity=Specificity)= %0.4f\n',SeSp-COEFF),
-        fprintf('4) Max Efficiency Cut-off point= %0.4f\n',CEff)
-        fprintf('5) Max PLR Cut-off point= %0.4f\n',CPlr)
-        fprintf('6) Max NLR Cut-off point= %0.4f\n',CNlr)
-        m=[CSe CSp SeSp CEff CPlr CNlr];
-        
     else
         m=NaN;
         matrix=NaN;
